5a7f9705c976466dce815ba5cdda7eaeffdec41c
Ztawh <amandahwatz@gmail.com>
2015-11-04 14:27:23 +0100

    Doesn't remove letter from alphabet

--- script.js
+++ script.js
@@ -4,28 +4,15 @@ document.addEventListener('DOMContentLoaded', function (){
 
 document.getElementById("start").addEventListener("click", playGame);
 
-var fragment = document.createDocumentFragment();
 
-function isLetter(keyCode){
+ 
+function isLetter(keyCode) {
     var character = String.fromCharCode(keyCode).toLowerCase();
     return character.length === 1 && character.match(/[a-z]/i);
-    //addValueToNode(letter);
 }
 
-//function addValueToNode(guessedLetter){    
-//    var nodes = document.getElementsByClassName(guessedLetter);
-//    if(nodes){
-
-//        for (var i = 0; i < nodes.length; i++){
-//            nodes[i].textContent = guessedLetter;
-//        }   
-//    }
-//}
-
-function playGame() {
-    var availableWords = ["schnitzel", "juice", "crocodile", "kurosaki"];
-    
-    var word = availableWords[Math.floor(Math.random() * availableWords.length)];
+function addUnderscore(word) {
+    var fragment = document.createDocumentFragment();
 
     for (var i=0; i < word.length; i++) {
         var div = document.createElement("div");
@@ -33,49 +20,110 @@ function playGame() {
         div.textContent = "_";
         fragment.appendChild(div);
     }
+    var content = document.getElementsByClassName("content")[0];
+    content.appendChild(fragment);
+}
+
+function youWon(word) {
+    var fragment = document.createDocumentFragment();
+
+    var winner = document.createElement("winner");
+    winner.className = "winner";
+    winner.textContent = "Congratulations! You won the game!";
+    fragment.appendChild(winner);
 
     var content = document.getElementsByClassName("content")[0];
     content.appendChild(fragment);
+}
+
+function availableLetters(alphabet){
+    var fragment = document.createDocumentFragment();
+
+    var alphabetInfo = document.createElement("div");
+    alphabetInfo.className = "alphabet-info";
+    alphabetInfo.textContent = "Please type a letter from the available ones below.";
+    fragment.appendChild(alphabetInfo);
+
+    for(var i=0; i < alphabet.length; i++){
+        var theAlphabet = document.createElement("div");
+        theAlphabet.className = "the-alphabet " + alphabet[i];
+        theAlphabet.textContent = alphabet[i];
+
+        fragment.appendChild(theAlphabet);
+    }
+    var content = document.getElementsByClassName("content")[0];
+    content.appendChild(fragment);
+
+}
 
 
-    var guessedLetter;
+
+function playGame() {
+    var availableWords = ["schnitzel", "juice", "crocodile", "kurosaki"];
+    var word = availableWords[Math.floor(Math.random() * availableWords.length)];
+
+    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+    availableLetters(alphabet);
+    addUnderscore(word);
+
+    var guessedKeyCodes = [];
     var keyCode;
-    var letter;
-    window.addEventListener("keyup", function (event) {
-        //isLetter(event.keyCode);
+    
 
-        keyCode = event.keyCode;
-
-        if(isLetter(keyCode)){
-           letter = String.fromCharCode(keyCode).toLowerCase();
-
-        }
-
-        //alert(letter);
+    var lettersInWord = word.split("").filter(function (element, position){
+        return word.indexOf(element) == position;
     });
 
-    alert(letter);
-   
-    var nodes = document.getElementsByClassName(letter);
-    if(nodes){
+    window.addEventListener("keyup", function (event) {
+        keyCode = event.keyCode;
+        var letter;
 
-        for (var i = 0; i < nodes.length; i++){
-            nodes[i].textContent = letter;
-        }   
-    }
+        if(isLetter(keyCode)){
+            //letter = String.fromCharCode(keyCode).toLowerCase();
+            //var index = alphabet.indexOf(letter);
+            //alphabet.splice(index, 1);
+            
+            if(guessedKeyCodes.indexOf(keyCode) !== -1){
+            alert("You have already tried that letter, please select from the remaining letters.");
+           }
+        }
 
+        guessedKeyCodes.push(keyCode);
 
-    //guessedLetter = isLetter(letter);
+        var letter = String.fromCharCode(keyCode).toLowerCase();
 
-    //alert(guessedLetter);
+        //CHANGE THIS TO GET VALUES IN DOCUMENT the-alphabet AND REWRITE DE VALUE
+        //var index = alphabet.indexOf(letter);
+        //alphabet.splice(index, 1);
 
-    //var nodes = document.getElementsByClassName(guessedLetter);
-    //if(nodes){
+        
+        //var parent = document.getElementsByTagName("content");
+        var letterClass = document.getElementsByClassName("the-alphabet");
+        for (var i=0; i<letterClass.length; i++){
+            if(letterClass[i] == letter){
+                letterClass[i].textContent = " ";
+                //parent.removeChild(letter);
+            }
+        }
 
-    //    for (var i = 0; i < nodes.length; i++){
-    //        nodes[i].textContent = guessedLetter;
-    //    }   
-    //}
+        if(word.indexOf(letter) !== -1){
+            var nodes = document.getElementsByClassName(letter);
+            for (var i = 0; i < nodes.length; i++){
+                nodes[i].textContent = letter;
+            }   
 
+            var letterPosition = lettersInWord.indexOf(letter);
+            lettersInWord.splice(letterPosition, 1);
+
+            //availableLetters(alphabet);
+
+            if (lettersInWord.length == 0)
+                youWon(word);
+        }
+
+        
+
+    });
 
 }
